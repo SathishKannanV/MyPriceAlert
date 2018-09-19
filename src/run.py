e853803e04bad007070fca77187d85c3961485cb
SathishKannanV <43075430+SathishKannanV@users.noreply.github.com>
2018-09-20 01:18:10 +0530

    update

--- src/run.py
+++ src/run.py
@@ -1,9 +1,8 @@
 from src.app import app
 import os
 
-port = int(os.environ.get('PORT', 5000))
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=port)
-
 # app.run(debug=app.config['DEBUG'], port=4990)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
