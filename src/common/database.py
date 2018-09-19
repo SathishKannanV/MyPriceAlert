c52e3fbdd87509f8193a6911be77f3b2b134d936
SathishKannanV <43075430+SathishKannanV@users.noreply.github.com>
2018-09-20 02:40:45 +0530

    Setting Default Heroku DB

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['heroku_test_db']
+        Database.DATABASE = client['heroku_41gjstr7']
         # Database.DATABASE = client.get_database()
 
     @staticmethod
