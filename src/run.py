467414605d433e1256c6e7b8baf856392b8d6326
SathishKannanV <43075430+SathishKannanV@users.noreply.github.com>
2018-09-20 01:04:20 +0530

    Update config

--- src/run.py
+++ src/run.py
@@ -2,6 +2,8 @@ from src.app import app
 import os
 
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=port)
 
 # app.run(debug=app.config['DEBUG'], port=4990)
