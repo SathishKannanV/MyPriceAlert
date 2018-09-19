a7dea1a9ca5be58795fd4d24a1dc0600638ea110
SathishKannanV <43075430+SathishKannanV@users.noreply.github.com>
2018-09-20 02:05:23 +0530

    Revert code

--- src/run.py
+++ src/run.py
@@ -5,4 +5,5 @@ import os
 
 # Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+app.run(debug=app.config['DEBUG'], port=port)
+# app.run(host='0.0.0.0', port=port)
