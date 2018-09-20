eb03904810c106c494f1e8fdadf3f0dd3bb0ded2
SathishKannanV <43075430+SathishKannanV@users.noreply.github.com>
2018-09-20 06:11:07 +0530

    Correcting URI

--- src/common/database.py
+++ src/common/database.py
@@ -5,8 +5,7 @@ import pymongo
 
 class Database(object):
     # URI = "mongodb://127.0.0.1:27017"
-    URI = os.environ.get("MONGOLAB.URI")
-    print(URI)
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
 
     @staticmethod
